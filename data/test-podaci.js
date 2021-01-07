6bb4addc1646a9e521d8b8609c2929af8399cb64
gzaharija-pmfst <71737050+gzaharija-pmfst@users.noreply.github.com>
2021-01-07 11:46:45 +0100

    Update test-podaci.js

--- data/test-podaci.js
+++ data/test-podaci.js
@@ -4,7 +4,7 @@ export const KATEGORIJE = [
   new Kategorija('c1', 'Talijanska', '#f5428d'),
   new Kategorija('c2', 'Brzo i jednostavno', '#f54242'),
   new Kategorija('c3', 'Burgeri', '#f5a442'),
-  new Kategorija('c4', 'Pizze', '#f5d142'),
+  new Kategorija('c4', 'Njemačka', '#f5d142'),
   new Kategorija('c5', 'Lagana jela', '#368dff'),
   new Kategorija('c6', 'Egzotična', '#41d95d'),
   new Kategorija('c7', 'Doručak', '#9eecff'),
